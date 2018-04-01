b319edfc9f636b6ea64427eee70247c97b196194
naren <narenravi92@gmail.com>
2018-04-01 17:50:47 +0530

    Add basic webserver in backend

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
-	"strconv"
 	"time"
+	"strconv"
 )
 
 var responses chan struct {
@@ -13,29 +12,60 @@ var responses chan struct {
 	ResponseTime   string
 }
 
-func main() {
-	wasps, _ := strconv.Atoi(os.Args[1])
-	swarms, _ := strconv.Atoi(os.Args[2])
+type attack struct {
+	Swarms int	`json:"swarms"`
+	Wasps int	`json:"wasps"`
+}
 
+func main() {
 	fmt.Println("yellowjacket welcomes you. buzzzzz..")
 	responses = make(chan struct {
 		ResponseHeader *http.Response
 		ResponseTime   string
 	})
-	for i := 0; i < wasps; i++ {
-		go Get(swarms, "http://www.google.com")
-	}
-
-	func() {
+	go func() {
 		for resp := range responses {
 			fmt.Println(resp.ResponseHeader.StatusCode, " took ", resp.ResponseTime)
 		}
 		fmt.Println("Blocking receiver")
 	}()
+	http.HandleFunc("/attack", AttackHander) // set router
+	err := http.ListenAndServe(":8000", nil) // set listen port
+	if err != nil {
+		fmt.Println("ListenAndServe: ", err)
+	}
+	fmt.Println("Listening on port 8000: ")
 }
 
-func Get(swarm int, url string) {
+func AttackHander(w http.ResponseWriter, r *http.Request) {
+	//decoder := json.NewDecoder(r.Body)
+	//fmt.Println("Got post form : ", r.Body)
+	//var a attack
+	//if err := decoder.Decode(&a); err != nil {
+	//	w.WriteHeader(http.StatusNotAcceptable)
+	//	w.Write([]byte("Unable to parse json"))
+	//	return
+	//}
+
+	r.ParseForm()
+	swarms, _ := strconv.Atoi(r.FormValue("swarms"))
+	wasps, _ := strconv.Atoi(r.FormValue("wasps"))
+	defer r.Body.Close()
+	fmt.Println("wasp count: ", wasps, swarms)
+	go createSwarm(swarms, wasps)
+}
+
+func createSwarm(swarms int, wasps int) {
+	fmt.Println("Into CreateSwarm")
+	for i := 0; i < wasps; i++ {
+		fmt.Println("Wasping")
+		AttackGet(swarms, "http://www.google.com")
+	}
+}
+
+func AttackGet(swarm int, url string) {
 	for i := 0; i < swarm; i++ {
+		fmt.Println("Attacking : ", swarm, url)
 		start := time.Now()
 		response, _ := http.Get(url)
 		elapsed := time.Since(start)
