facbffb0a4a4c7b0c281b191219d149a411b710a
naren <narenravi92@gmail.com>
2018-04-01 13:10:20 +0530

    Add Get function and use channels to communicate

--- main.go
+++ main.go
@@ -1,9 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
 
-func main() {
-	fmt.Println("Hola")
+var responses chan struct {
+	ResponseHeader *http.Response
+	ResponseTime   string
 }
 
+func main() {
+	wasps, _ := strconv.Atoi(os.Args[1])
+	swarms, _ := strconv.Atoi(os.Args[2])
 
+	fmt.Println("yellowjacket welcomes you. buzzzzz..")
+	responses = make(chan struct {
+		ResponseHeader *http.Response
+		ResponseTime   string
+	})
+	for i := 0; i < wasps; i++ {
+		go Get(swarms, "http://www.google.com")
+	}
+
+	func() {
+		for resp := range responses {
+			fmt.Println(resp.ResponseHeader.StatusCode, " took ", resp.ResponseTime)
+		}
+		fmt.Println("Blocking receiver")
+	}()
+}
+
+func Get(swarm int, url string) {
+	for i := 0; i < swarm; i++ {
+		start := time.Now()
+		response, _ := http.Get(url)
+		elapsed := time.Since(start)
+
+		responseStruct := struct {
+			ResponseHeader *http.Response
+			ResponseTime string
+
+		}{
+			response,
+			elapsed.String(),
+		}
+		responses <- responseStruct
+	}
+}
