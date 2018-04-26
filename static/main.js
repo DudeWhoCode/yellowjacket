2bdc630b0248f651a6f4af35984978389fa674fa
naren <narenravi92@gmail.com>
2018-04-26 21:00:57 +0530

    Add basic javascript

---
+++ static/main.js
@@ -0,0 +1,12 @@
+var start_swarm;
+var swarm_button;
+
+function swarm() {
+    start_swarm.style.display = 'block';
+}
+
+window.onload = function () {
+   start_swarm = document.getElementById("start-swarm");
+   swarm_button = document.getElementById('new-test');
+};
+
