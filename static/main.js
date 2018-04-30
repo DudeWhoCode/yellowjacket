748a5d10bd7f1ad0cd47e272f100087545df283f
naren <narenravi92@gmail.com>
2018-04-30 09:51:18 +0530

    Call the backend API to start load testing

--- static/main.js
+++ static/main.js
@@ -10,3 +10,16 @@ window.onload = function () {
    swarm_button = document.getElementById('new-test');
 };
 
+function attack() {
+    var xhttp = new XMLHttpRequest();
+    var waspsCount = document.getElementsByClassName('wasp');
+    var hatchRate = document.getElementsByClassName('swarm');
+    var payload = {};
+    payload.wasps = waspsCount;
+    payload.swarms = hatchRate;
+    xhttp.open("POST", "http://localhost:8000/attack", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(payload);
+    // var response = JSON.parse(xhttp.responseText);
+}
+
