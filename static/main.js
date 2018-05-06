f60749d7744b9736e70a3602ba6bf37de1f7b2ff
naren <narenravi92@gmail.com>
2018-05-06 11:58:25 +0530

    Correct the post request

--- static/main.js
+++ static/main.js
@@ -28,11 +28,17 @@ function attack() {
     var waspsCount = document.getElementsByClassName('wasp')[0].value;
     var hatchRate = document.getElementsByClassName('swarm')[0].value;
     var payload = {};
-    payload.wasps = waspsCount;
-    payload.swarms = hatchRate;
+    payload.wasps = Number(waspsCount);
+    payload.swarms = Number(hatchRate);
+    console.log(
+        'Payload: ', payload
+    );
     xhttp.open("POST", "http://localhost:8000/attack", true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(payload);
+    xhttp.send(JSON.stringify(payload));
+    console.log('Before toggling');
+    toggle_box();
+    console.log('After toggling')
     // var response = JSON.parse(xhttp.responseText);
 }
 
