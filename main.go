7805e3e8b40a3b12e711d18f8d5cd3d06cd4c68f
naren <narenravi92@gmail.com>
2018-06-12 11:54:11 +0530

    Start SSE broker

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/DudeWhoCode/yellowjacket/backend"
 	"github.com/DudeWhoCode/yellowjacket/server"
 	"github.com/rs/cors"
 )
@@ -14,5 +15,15 @@ func main() {
 	fs := http.FileServer(http.Dir("templates/"))
 	rt.PathPrefix("/templates/").Handler(http.StripPrefix("/templates/", fs))
 	// http.Handle("/", rt)
+	b := backend.GetBroker()
+	// Start processing events
+	b.Start()
+
+	// Make b the HTTP handler for "/events/".  It can do
+	// this because it has a ServeHTTP method.  That method
+	// is called in a separate goroutine for each
+	// request to "/events/".
+	rt.Handle("/events/", b)
+
 	log.Fatal(http.ListenAndServe(":8001", handler))
 }
