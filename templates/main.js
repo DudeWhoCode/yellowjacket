e4b95ef89e3139185e0d9e79b3a952f33914f114
naren <narenravi92@gmail.com>
2018-06-12 11:55:41 +0530

    Handle server sent events through eventstream

--- templates/main.js
+++ templates/main.js
@@ -17,10 +17,29 @@ function toggle_box() {
 
 }
 
+function handle_sse() {
+    // Create a new HTML5 EventSource
+    var source = new EventSource('/events/');
+    // Create a callback for when a new message is received.
+    source.onmessage = function(e) {
+
+        // Append the `data` attribute of the message to the DOM.
+        // document.body.innerHTML += e.data + '<br>';
+        var ip_array = e.data.split(',');
+        sumReq = ip_array[0]
+        sumFail = ip_array[1]
+        avgLatency = ip_array[2]
+        console.log(e)
+        document.getElementById("sum-success").innerHTML = sumReq
+        document.getElementById("sum-fail").innerHTML = sumFail
+        document.getElementById("avg-latency").innerHTML = avgLatency
+    };
+}
 
 window.onload = function () {
    start_swarm = document.getElementById("start-swarm");
    swarm_button = document.getElementById('new-test');
+   handle_sse()
 };
 
 function attack() {
