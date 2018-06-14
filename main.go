ef786d9cc81eb7ca4f51faecea99a716d8bc24ba
naren <narenravi92@gmail.com>
2018-06-14 09:02:42 +0530

    Remove comments

--- main.go
+++ main.go
@@ -14,16 +14,8 @@ func main() {
 	handler := cors.Default().Handler(rt)
 	fs := http.FileServer(http.Dir("templates/"))
 	rt.PathPrefix("/templates/").Handler(http.StripPrefix("/templates/", fs))
-	// http.Handle("/", rt)
 	b := backend.GetBroker()
-	// Start processing events
 	b.Start()
-
-	// Make b the HTTP handler for "/events/".  It can do
-	// this because it has a ServeHTTP method.  That method
-	// is called in a separate goroutine for each
-	// request to "/events/".
-	rt.Handle("/events/", b)
 
 	log.Fatal(http.ListenAndServe(":8001", handler))
 }
