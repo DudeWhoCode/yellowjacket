64cbbe3fe70b4d5fa9180d648428ffb12b1ecd81
naren <narenravi92@gmail.com>
2018-07-03 08:57:39 +0530

    Execute the command

--- yellowjacket.go
+++ yellowjacket.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"github.com/DudeWhoCode/yellowjacket/backend"
+	"github.com/DudeWhoCode/yellowjacket/cmd"
 )
 
 func main() {
 	backend.LoadModule()
-	// cmd.Execute()
+	cmd.Execute()
 }
