2547b8cad829a08a2f56e60c0b2e613bc4494fa4
naren <narenravi92@gmail.com>
2018-07-31 08:51:05 +0530

    Add more tests

--- yellow-plugins/testfile.go
+++ yellow-plugins/testfile.go
@@ -11,12 +11,17 @@ type Task backend.User
 
 func (u *Task) Foo() (*http.Response, error) {
 	fmt.Println("Inside foo")
-	return u.Client.Get("http://google.com")
+	return u.Client.Get("https://www.twitter.com/dudewhocode")
 }
 
 func (u *Task) Bar() (*http.Response, error) {
 	fmt.Println("Inside bar")
-	return u.Client.Get("http://google.com")
+	return u.Client.Get("https://www.twitter.com/spf13")
+}
+
+func (u *Task) Baz() (*http.Response, error) {
+	fmt.Println("Inside baz")
+	return u.Client.Get("https://www.twitter.com/cnu")
 }
 
 func GetTask() *Task {
